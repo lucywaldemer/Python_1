35763ecf213301c1023c3994f21ec803852e053e
Lu Waldemer <lucywaldemer@gmail.com>
2023-10-30 10:36:40 -0500

    adding all files

---
+++ Variables.py
@@ -0,0 +1,7 @@
+character_name = "Lu"
+character_age = "65"
+
+print("There once was a man named " + character_name + ",")
+print("and he was " + character_age + " years old.")
+print("He liked the name " + character_name + ",")
+print("but he didn't like being " + character_age + ".")
