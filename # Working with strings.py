542077b892ce34c94678f2630c085aed2ce38515
Lu Waldemer <lucywaldemer@gmail.com>
2023-11-01 12:16:23 -0500

    working with strings

---
+++ # Working with strings.py
@@ -0,0 +1,21 @@
+# Working with strings
+print("Giraffe Academy")
+print("\"")
+
+#set it at a variable
+phrase = "This is a phrase"
+print(phrase + " that says hello.")
+
+# can do case changes and booleans for isupper or islower
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.upper().isupper())
+
+# len is the length function
+print(len(phrase))
+
+# individual characters, index starts at 0
+print(phrase[11])
+
+# index funtion
+print(phrase.index("a"))
