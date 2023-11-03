024bb4200aa87e51e71095fcc4acd01d87edcf7c
Lu Waldemer <lucywaldemer@gmail.com>
2023-11-03 10:06:38 -0500

    numbers and math import

---
+++ numbers.py
@@ -0,0 +1,33 @@
+from math import *
+# need to import the math functions, documentation is online
+
+# working with numbers
+
+#can do simple operations, follows PEMDAS
+print(2 + 5)
+
+#modulous AKA division remainder
+print(10%3)
+
+#can set as a variable & change types
+my_num=5
+print(str(my_num) + " is my fav number")
+
+#pow is power ex: 3 squared
+print(pow(3,2))
+
+#max and max
+print(min(80,4))
+print(max(80,4))
+
+#rounding to nearest whole number
+print(round(3.736272))
+
+#floor rounds down only
+print(floor(3.7))
+
+#ceil rounds up only
+print(ceil(9.2))
+
+#square root
+print(sqrt(36))
